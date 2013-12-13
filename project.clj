f6b9e77e50647c06bcff961e6ac0af09955a533f
Rosnec <rosnec.dev@gmail.com>
2013-12-13 00:16:20 -0500

    Releasing v0.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject visualloy "0.3.0-SNAPSHOT"
+(defproject visualloy "0.3.0"
   :description "Computes heat transfer across a metal alloy and displays it
                graphically"
   :url "https://github.com/Rosnec/visualloy"
@@ -6,5 +6,4 @@
             :url  "https://www.gnu.org/licenses/gpl-3.0.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [seesaw "1.4.4"]]
-  :main visualloy.core
-  :jvm-opts ["-verbosegc"])
+  :main visualloy.core)
